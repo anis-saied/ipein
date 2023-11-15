9bee05bbdb8bd191a229c1b8c745993cbead76f5
Anis SAIED <anissaied2@gmail.com>
2023-11-15 12:15:48 +0100

    Update convert_py_to_pdf.py

--- scripts/convert_py_to_pdf.py
+++ scripts/convert_py_to_pdf.py
@@ -6,33 +6,38 @@ from pygments.formatters import HtmlFormatter
 from xhtml2pdf import pisa
 
 def convert_python_to_pdf(input_file, output_file):
-    # Read the Python code from the input file
-    with open(input_file, 'r') as file:
-        python_code = file.read()
+    try:
+        # Read the Python code from the input file
+        with open(input_file, 'r') as file:
+            python_code = file.read()
 
-    # Create HTML content with syntax highlighting
-    formatter = HtmlFormatter(style='vs')#colorful
-    highlighted_code = highlight(python_code, PythonLexer(), formatter)
+        # Create HTML content with syntax highlighting
+        formatter = HtmlFormatter(style='vs')  # or use 'colorful'
+        highlighted_code = highlight(python_code, PythonLexer(), formatter)
 
-    # Prepare HTML content
-    html_content = f"""
-    <html>
-    <head>
-        <style>{formatter.get_style_defs()}</style>
-    </head>
-    <body>
-        {highlighted_code}
-    </body>
-    </html>
-    """
+        # Prepare HTML content
+        html_content = f"""
+        <html>
+        <head>
+            <style>{formatter.get_style_defs()}</style>
+        </head>
+        <body>
+            {highlighted_code}
+        </body>
+        </html>
+        """
 
-    # Generate PDF from HTML content
-    with open(output_file, 'w+b') as pdf_file:
-        pisa.pisaDocument(html_content, pdf_file)
+        # Generate PDF from HTML content
+        with open(output_file, 'w+b') as pdf_file:
+            pisa.pisaDocument(html_content, pdf_file)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python convert_py_to_pdf.py input_file.py output_file.pdf")
+        print("Usage: python convert_py_to_pdf.py input_file.py")
         sys.exit(1)
 
     input_file = sys.argv[1]
